711391956c95188353ec331f75b7071fd424d636
Елена Краснобрыж <unipurrr@gmail.com>
2020-11-19 22:35:08 +0300

    TrainingPractice_01
    TrainingPractice_01

---
+++ KES_Task_05/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KES_Task_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ KES_Task_07/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace KES_Task_07
+{
+    class Program
+    {
+        private static void Shuffle(int[] arr)
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 100; i++)
+            {
+                var rndInd = rnd.Next(0, arr.Length);
+                var rndInd2 = rnd.Next(0, arr.Length);
+                var temp = arr[rndInd];
+                arr[rndInd] = arr[rndInd2];
+                arr[rndInd2] = temp;
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            var mass1 = new[] { 1, 22, 3, 4, 5, 9, 8, 24, 38, 1, 54, 63, 2 };
+            int[] mass2 = mass1;
+            Console.WriteLine("Первый массив:");
+            Console.WriteLine(string.Join(" ", mass1));
+            Shuffle(mass2);
+            Console.WriteLine("Второй массив:");
+            Console.WriteLine(string.Join(" ", mass2));
+            Console.ReadKey();
+        }
+    }
+}
